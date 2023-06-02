bf1b93c270a75f2123ac5098bd41982b51c42c58
Onyeka Obi <oo22aap@herts.ac.uk>
2023-06-02 04:17:33 +0100

    Worked on individual file compression

--- main.py
+++ main.py
@@ -12,42 +12,67 @@ def is_valid_image(file: str):
         return False
 
 
-def get_path():
+def compress(file_path):
+    while True:
+
+        quality: str = input("Specify quality (1 - 100)\n>>> ")
+        if quality.isdigit() and int(quality) in range(1, 101):
+            pass
+        else:
+            continue
+
+        if type(file_path) is str:
+            with Image.open(file_path) as im:
+                image = file_path.split('.')[0]
+                file_format = file_path.split('.')[1]
+
+                output_file: str = f"{image}_compressed.{file_format}"
+
+                im.save(output_file, optimize=True, quality=int(quality))
+
+        return "list"
+
+
+def get_path(choice: str):
+    if choice == '1':
+        path: str = askopenfilename()
+        if is_valid_image(path):
+            return path
+        return None
+
+    if choice == '2':
+        paths_list: list = []
+        paths: str = askopenfilenames()
+        for path in paths:
+            if is_valid_image(path):
+                paths_list.append(path)
+
+        return paths_list
+
+    if choice == '3':
+        paths_list: list = []
+        directory_path: str = askdirectory()
+
+        for path in os.listdir(directory_path):
+            file_path: str = os.path.join(directory_path, path)
+            if is_valid_image(file_path):
+                paths_list.append(file_path)
+
+        return paths_list
+
+
+if __name__ == '__main__':
     while True:
         user_choice: str = input("Press '1' to select a file\nPress '2' to select multiple files"
                                  "\nPress '3' to select a folder\nPress '4' to exit\n>>> ")
 
-        if user_choice == '1':
-            path: str = askopenfilename()
-            if is_valid_image(path):
-                return path
-            return None
-
-        if user_choice == '2':
-            paths_list: list = []
-            paths: str = askopenfilenames()
-            for path in paths:
-                if is_valid_image(path):
-                    paths_list.append(path)
-
-            return paths_list
-
-        if user_choice == '3':
-            paths_list: list = []
-            directory_path: str = askdirectory()
-
-            for path in os.listdir(directory_path):
-                file_path: str = os.path.join(directory_path, path)
-                if is_valid_image(file_path):
-                    paths_list.append(file_path)
-
-            return paths_list
+        if user_choice.isdigit() and int(user_choice) in range(1, 4):
+            user_path = get_path(user_choice)
+            compress(user_path)
+            break
 
         if user_choice == '4':
-            return "Thank you, Goodbye"
+            print("Thank you, Goodbye")
+            break
 
         print("Sorry, Invalid Entry\n")
-
-
-if __name__ == '__main__':
-    print(get_path())
